75fe129e6e45d5aaf7f35f86dd09c6277ec13320
Prajen2004 <137319196+Prajen2004@users.noreply.github.com>
2025-06-30 22:52:59 +0530

    Update health_chatbot.py

--- health_chatbot.py
+++ health_chatbot.py
@@ -573,7 +573,7 @@ def main():
         password = st.text_input("Password:", type="password")
         
         if st.button("Login"):
-            if username == "Prajen SK" and password == "Prajenram@2004":
+            if username == "admin" and password == "1234":
                 st.session_state.authenticated = True
                 st.session_state.username = username
                 st.success("Logged in successfully!")
